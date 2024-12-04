d4ab626857b20c057ae835e343104e597388c3bb
Michael Lim <michaeljoelmama@gmail.com>
2024-12-04 14:58:49 +0700

    oke udah bisa

--- main.c
+++ main.c
@@ -4,15 +4,15 @@ int main(int argc, char *argv[]) {
 	
 	
 	
-	printf("     ");
-	printf("  000");
-	printf("     ");
-	printf("     ");
-	printf("  000");
-	printf("  000");
-	printf("  000");
-	printf("  000");
-	printf("  000");
+	printf("     			");
+	printf("  000		000 ");
+	printf("  000		000 ");
+	printf("    			");
+	printf("  0000000000000 ");
+	printf("  000		000 ");
+	printf("  000		000 ");
+	printf("  000		000 ");
+	printf("  000		000 ");
 
 	
 	return 0;
