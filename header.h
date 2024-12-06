bd3e1c843ecec6fc56dbdb76e3699f021bc2ad5a
Michael Lim <michaeljoelmama@gmail.com>
2024-12-06 14:34:04 +0700

    header part 1

--- header.h
+++ header.h
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+typedef char string[100];
+typedef struct Child* AddressChild;
+typedef struct Parent* AddressParent;
 
+typedef struct DataChild{
+	string a;
+	int b;
+}DataChild;
 
+typedef struct DataParent{
+	string a;
+	int b;
+}DataParent;
 
+typedef struct Child{
+	DataChild datachild;
+	AddressChild next;
+}NodeChild;
+
+typedef struct Parent{
+	DataParent dataparent;
+	AddressParent next;
+	
+	AddressChild firstChild;
+}NodeParent;
+
+typedef struct {
+	AddressParent firstParent;
+}Multilist;
